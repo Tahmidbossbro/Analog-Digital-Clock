92330ac107e5362b073b87b428dfb3c7d8956c14
Tahmid Newaz <tahmidnewazisfar@gmail.com>
2023-09-13 15:54:16 +0600

    Set up clock view for different screens

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // const h1Tag = document.querySelector('h1') as HTMLElement;
-
 // h1Tag.innerText = "Hello There";
 // window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
 //     const portrait = e.matches;
-
 //     if (portrait) {
 //         // do something
 //     } else {
