3b395b4487438d85b46c2d69dd7060eb79faa5ef
Tahmid Newaz <tahmidnewazisfar@gmail.com>
2023-09-15 21:31:47 +0600

    Added time function to both Analog and Digital Clocks

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-// const h1Tag = document.querySelector('h1') as HTMLElement;
-// h1Tag.innerText = "Hello There";
-// window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
-//     const portrait = e.matches;
-//     if (portrait) {
-//         // do something
-//     } else {
-//         // do something else
-//     }
-// });
-// const clock = document.querySelector('.clock') as HTMLInputElement;
-// for (let i = 0; i < 12; i++) {
-//     clock.innerHTML += '<h1 style="color:red;" class = No'+ (i+1) +'>' + (i+1) +'</h1>'
-// }
+var secondHand = document.querySelector('.second-hand');
+var minuteHand = document.querySelector('.minute-hand');
+var hourHand = document.querySelector('.hour-hand');
+var secondDigit = document.querySelector('.second-digit');
+var minuteDigit = document.querySelector('.minute-digit');
+var hourDigit = document.querySelector('.hour-digit');
+secondHand.style.transform = 'rotate(60deg)';
+setInterval(myMethod, 500);
+function myMethod() {
+    var d = new Date();
+    // console.log(d.getHours + " : " + d.getMinutes + " : " + d.getSeconds);
+    var seconds = d.getSeconds();
+    var minutes = d.getMinutes();
+    var hours = d.getHours();
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    if (seconds < 10) {
+        secondDigit.innerText = "0" + seconds.toString();
+    }
+    else {
+        secondDigit.innerText = seconds.toString();
+    }
+    if (minutes < 10) {
+        minuteDigit.innerText = "0" + minutes.toString();
+    }
+    else {
+        minuteDigit.innerText = minutes.toString();
+    }
+    if (hours < 10) {
+        hourDigit.innerText = "0" + hours.toString();
+    }
+    else {
+        hourDigit.innerText = hours.toString();
+    }
+    secondHand.style.transform = 'rotate(' + (seconds * 6) + 'deg)';
+    minuteHand.style.transform = 'rotate(' + ((6 * minutes) + (seconds * 0.1)) + 'deg)';
+    hourHand.style.transform = 'rotate(' + ((hours * 30) + (minutes * 0.5)) + 'deg)';
+}
--- index.ts
+++ index.ts
@@ -1,19 +1,58 @@
-// const h1Tag = document.querySelector('h1') as HTMLElement;
+const secondHand = document.querySelector('.second-hand') as HTMLElement;
+const minuteHand = document.querySelector('.minute-hand') as HTMLElement;
+const hourHand = document.querySelector('.hour-hand') as HTMLElement;
 
-// h1Tag.innerText = "Hello There";
-// window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
-//     const portrait = e.matches;
+const secondDigit = document.querySelector('.second-digit') as HTMLInputElement;
+const minuteDigit = document.querySelector('.minute-digit') as HTMLInputElement;
+const hourDigit = document.querySelector('.hour-digit') as HTMLInputElement;
 
-//     if (portrait) {
-//         // do something
-//     } else {
-//         // do something else
-//     }
-// });
-// const clock = document.querySelector('.clock') as HTMLInputElement;
 
-// for (let i = 0; i < 12; i++) {
+secondHand.style.transform = 'rotate(60deg)';
+
+
+setInterval(myMethod, 500);
+
+function myMethod( )
+{
     
-//     clock.innerHTML += '<h1 style="color:red;" class = No'+ (i+1) +'>' + (i+1) +'</h1>'
+    const d = new Date();
+    // console.log(d.getHours + " : " + d.getMinutes + " : " + d.getSeconds);
+    
+    let seconds = d.getSeconds();
+    let minutes = d.getMinutes();
+    let hours = d.getHours();
 
-// }
+    if (hours > 12){hours = hours -12 ;}
+
+    if (seconds < 10){
+        secondDigit.innerText = "0" + seconds.toString();
+    }
+    else{
+        secondDigit.innerText = seconds.toString();
+    }
+  
+    if (minutes < 10){
+        minuteDigit.innerText = "0" + minutes.toString();
+    }
+    else{
+        minuteDigit.innerText = minutes.toString();
+    }
+
+    if (hours < 10){
+        hourDigit.innerText = "0" + hours.toString();
+    }
+    else{
+        hourDigit.innerText = hours.toString();
+    }
+    
+    
+    
+
+    secondHand.style.transform = 'rotate('+(seconds*6)+'deg)';
+    minuteHand.style.transform = 'rotate('+((6*minutes) +(seconds*0.1))+'deg)';
+    hourHand.style.transform = 'rotate('+((hours * 30 ) + (minutes * 0.5))+'deg)';
+
+
+    
+    
+}
